cfcf5b94b8f8a000db425168f87f3a0191881be7
srinath015 <2403a51015@sru.edu.in>
2025-08-28 13:52:05 +0530

    Add files via upload

---
+++ LAB 7/TASK3.1_CU.py
@@ -0,0 +1,2 @@
+with open(r"C:\Users\pooda\OneDrive\Desktop\AI_28.txt", "w") as f:
+    f.write("Hello, world.")
---
+++ LAB 7/TASK3.2_VS.py
@@ -0,0 +1,7 @@
+f1 = open(r"C:\Users\pooda\OneDrive\Desktop\AI_28.txt", "w")
+f2 = open(r"C:\Users\pooda\OneDrive\Desktop\AI_28.1.txt", "w")
+f1.write("First file content\n")
+f2.write("Second file content\n")
+f1.close()
+f2.close()
+print("Files written successfully.")
